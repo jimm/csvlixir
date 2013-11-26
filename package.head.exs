419d86f3a9956b5bcb890ca7a75a9c5219317725
Jim Menard <jim.menard@nrelate.com>
2013-11-26 18:00:45 -0500

    Expm package description.

---
+++ package.head.exs
@@ -0,0 +1,6 @@
+Expm.Package.new(name: "csvlixir",
+                 description: "Elixir CSV reader and writer",
+                 version: :head,
+                 keywords: ["CSV","data","text", "Elixir"], 
+                 maintainers: [[name: "Jim Menard", email: "jim@jimmenard.com"]],
+                 repositories: [[github: "jimm/csvlixir"]])
