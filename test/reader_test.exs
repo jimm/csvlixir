140af53e27dfb366bb0ad0010de0472c50da4b95
Jim Menard <jim@jimmenard.com>
2014-06-19 07:55:32 -0400

    A few more test cases. Simplified test code. Replaced tab chars with "\t" in test code.

--- test/reader_test.exs
+++ test/reader_test.exs
@@ -2,20 +2,29 @@ defmodule CsvReaderTest do
   use ExUnit.Case
 
   @test_input [
+    "hi!",
+    "1,really,simple,row",
     "row one,\"\"\"nasty:,\"\"\",\"has, comma\",\"has \"\"quotes\"\"\",has 'single quotes'",
     "\"has \"\"comma, in quotes\"\"\",\"has 'comma, in single quotes'\",\"quotes \"\",\"\" surrounding\",,",
     "\"odd \"\"number of\"\" double \"\"quotes\",\"two, yes two, commas\",\"two, \"\"commas, second\"\" in quotes\",,",
-    "simple,row,\"internal \"\"quote\",,"
+    "simple,row,\"internal \"\"quote\",,",
+    "\"this row continues\non the next line\",hi!"
   ]
 
-  # Expected results are tab-delimited. Lines that end with a backslash are
-  # continued on the next line.
+  # Expected results are tab-delimited.
   @test_expected [
-    "row one	\"nasty:,\"	has, comma	has \"quotes\"	has 'single quotes'",
-    "has \"comma, in quotes\"	has 'comma, in single quotes'	quotes \",\" surrounding		",
-    "odd \"number of\" double \"quotes	two, yes two, commas	two, \"commas, second\" in quotes		",
-    "simple	row	internal \"quote		"
+    "hi!",
+    "1\treally\tsimple\trow",
+    "row one\t\"nasty:,\"\thas, comma\thas \"quotes\"\thas 'single quotes'",
+    "has \"comma, in quotes\"\thas 'comma, in single quotes'\tquotes \",\" surrounding\t\t",
+    "odd \"number of\" double \"quotes\ttwo, yes two, commas\ttwo, \"commas, second\" in quotes\t\t",
+    "simple\trow\tinternal \"quote\t\t",
+    "this row continues\non the next line\thi!"
   ]
+
+  test "empty input" do
+    assert CSVLixir.read("") == []
+  end
 
   # Compare parsed @test_input with @test_expected.
   test "reader" do
@@ -27,13 +36,5 @@ defmodule CsvReaderTest do
     [row] = CSVLixir.read(input)
     expected_row = String.split(expected, "\t")
     assert row == expected_row
-  end
-
-  test "multi-line column" do
-    input = "\"this row continues\non the next line\""
-    expected = "this row continues\non the next line"
-
-    [[row]] = CSVLixir.read(input)
-    assert row == expected
   end
 end
